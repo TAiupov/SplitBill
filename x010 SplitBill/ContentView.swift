e36ecc4f43acd6bbe50d28efc2ba56d8d17e865e
Tatar <avtren@gmail.com>
2021-06-09 23:06:08 -0700

    Initial Commit

---
+++ x010 SplitBill/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  x010 SplitBill
+//
+//  Created by Тагир Аюпов on 2020-12-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var billAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercent = 2
+    
+    let tipPercentages = [10, 15, 20 , 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double (tipPercentages[tipPercent])
+        
+        let orderAmount = Double (billAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $billAmount).keyboardType(.decimalPad)
+                    
+                    Picker(selection: $numberOfPeople, label: Text("Number of People"), content: {
+                        ForEach(2..<100) {
+                            Text("\($0)")
+                        }
+                    })
+                }
+                
+                    Section(header: Text("How much tip do you want to leave?"), content: {
+                        Picker("Tip Percentage", selection: $tipPercent) {
+                            ForEach(0..<tipPercentages.count){
+                                Text("\(tipPercentages[$0])%")
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                    })
+                
+                
+                Section(header: Text("Total + Tip"), content: {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                    
+                })
+            }.navigationTitle("Bill $plit")
+        }.gesture(DragGesture().onChanged{_ in
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)})
+}
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ x010 SplitBill/x010_SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  x010_SplitBillApp.swift
+//  x010 SplitBill
+//
+//  Created by Тагир Аюпов on 2020-12-13.
+//
+
+import SwiftUI
+
+@main
+struct x010_SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
